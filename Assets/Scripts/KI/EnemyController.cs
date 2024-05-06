36a752a0e10438677d3eabc09335764d5fe688fa
JonasLink00 <jonas@link-family.de>
2024-05-06 15:45:35 +0200

    KI

--- Assets/Scripts/KI/EnemyController.cs
+++ Assets/Scripts/KI/EnemyController.cs
@@ -98,7 +98,7 @@ public class EnemyController : BaseController
                 attackState,
                 new Dictionary<StateMachineDelegate, EnemyBaseState>
                 {
-                    {() => Work < WorkThreshHold, walkState }
+                    
                 }
             },
 
