31f102d3d2ec054edc26512f0c99824811f325f8
JonasLink00 <jonas@link-family.de>
2024-05-29 14:51:34 +0200

    SimExplo

---
+++ Assets/Scripts/Wagon/Freight/shatteredBox.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class shatteredBox : MonoBehaviour
+{
+    [SerializeField] private GameObject box;
+    [SerializeField] private GameObject shattered;
+    [SerializeField] private float time = 0.3f;
+    private shatteredPartList partList;
+    private List<MeshRenderer> shatteredRendererList;
+    [SerializeField] private float DespawnTime = 5f;
+    private bool Despawned;
+    private float despawnTimer;
+    [SerializeField]
+    float contactForce = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        despawnTimer = DespawnTime;
+
+        if (other.GetComponent<Rigidbody>() != null)
+        {
+            BreakTheThing(other.transform.position);
+        }
+    }
+
+   
+    private void Update()
+    {
+        if (shattered.activeSelf)
+        {
+            despawnTimer -= Time.deltaTime;
+
+            if (despawnTimer <= 0)
+            {
+                despawnTimer = 0;
+                Despawned = true;
+                //Despawn(shattered);
+                shattered.SetActive(false);
+            }
+        }
+    }
+
+    public void BreakTheThing(Vector3 position)
+    {
+       
+        shattered.SetActive(true);
+        box.SetActive(false);
+        GetComponent<BoxCollider>().enabled = false;
+        partList = shattered.GetComponent<shatteredPartList>();
+        List<MeshRenderer> List = partList.GetPartList;
+       foreach(var part in List)
+       {
+            Rigidbody rb =part.GetComponent<Rigidbody>();
+
+            //diffrenceVector = target - origin
+
+            Vector3 diffrenceVector = rb.transform.position - position;
+            Vector3 directionVector = diffrenceVector.normalized;
+
+            rb.AddForce(directionVector * contactForce, ForceMode.Impulse);
+            //rb.velocity = directionVector * contactForce;
+            Debug.Log("AddForce" + rb.transform.position);
+
+       }
+
+        StartCoroutine(Blink(shattered, time));
+    }
+
+    IEnumerator Blink(GameObject despawnGO, float time)
+    {
+        //für jedes Mesh in der List renderer Aktivieren/Deaktivieren
+
+
+
+        foreach (MeshRenderer renderer in partList.GetPartList)
+        {
+            while (!Despawned)
+            {
+                renderer.enabled = false;
+                yield return new WaitForSeconds(time);
+                renderer.enabled = true;
+            }
+        }
+
+        //for (int i = 0; i <= 10; i++)
+        //  {
+        //      meshRenderer.enabled = false;
+        //      yield return new WaitForSeconds(time);
+        //      meshRenderer.enabled = true;
+        //  }
+    }
+    
+
+    private void Despawn(GameObject spawnedGO)
+    {
+        spawnedGO.SetActive(false);
+
+    }
+} 
---
+++ Assets/Scripts/Wagon/Freight/shatteredPartList.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class shatteredPartList : MonoBehaviour
+{
+    [SerializeField] List<MeshRenderer> partList;
+
+    public List<MeshRenderer> GetPartList { get => partList; }
+
+    private void Start()
+    {
+        PopulateList();
+    }
+
+    private void PopulateList()
+    {
+        partList = GetComponentsInChildren<MeshRenderer>().ToList<MeshRenderer>();
+        gameObject.SetActive(false);
+    }
+}
