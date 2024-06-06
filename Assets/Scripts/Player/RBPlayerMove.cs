cb0b62b9c7cc6cebe5aa51c48b564ba8ad193488
JonasLink00 <jonas@link-family.de>
2024-06-06 18:03:48 +0200

    help

---
+++ Assets/Scripts/Player/RBPlayerMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RBPlayerMove : MonoBehaviour
+{
+    private float speed = 200f;
+    private float rotationSpeed = 100f;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        _rigidbody.velocity = (transform.forward * vAxis) * speed * Time.deltaTime;
+        transform.Rotate((transform.up * hAxis) * rotationSpeed * Time.deltaTime);
+    }
+
+
+}
