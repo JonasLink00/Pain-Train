5ff61e36f374d537019276c4599ff311f2f21d29
JonasLink00 <jonas@link-family.de>
2024-09-05 12:19:37 +0200

    TrainShake

--- Assets/Scripts/Manager/TrainManager.cs
+++ Assets/Scripts/Manager/TrainManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class TrainManager : MonoBehaviour
@@ -7,6 +8,18 @@ public class TrainManager : MonoBehaviour
     [SerializeField] private EnemyController WorkingEnemy;
     [SerializeField] private EnemyController PassangerEnemy;
     [SerializeField] private AudioSource TrainSound;
+
+    [SerializeField] CameraShake camerashake;
+
+    bool checkShake = false;
+    private void Update()
+    {
+        //Reduce the aktivation of the coroutine 
+        if (!checkShake)
+        {
+            StartCoroutine(TrainShake());
+        }
+    }
 
     public void Playerspotted()
     {
@@ -33,4 +46,24 @@ public class TrainManager : MonoBehaviour
         TrainSound.volume = 0.15f;
     }
 
+
+    public IEnumerator TrainShake()
+    {
+        checkShake = true;
+        Debug.Log("Shake?");
+
+        int ShakeChance = Random.Range(0, 5);
+        Debug.Log(ShakeChance);
+
+        if (ShakeChance == 4)
+        {
+            Debug.Log("Shake!");
+            CameraShake.Shake(2f, 1f);
+            yield return new WaitForSeconds(5f);
+            checkShake = false;
+        }
+        yield return new WaitForSeconds(1f);
+        checkShake = false;
+    }
+    
 }
